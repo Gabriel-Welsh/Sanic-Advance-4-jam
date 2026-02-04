75626083695699d1d2cbcf133c8377122693dee7
Gabe Welsh <gjw4492@gmail.com>
2026-02-04 12:41:19 -0600

    sonic moves alright now

--- entities/sonic/sonic.gd
+++ entities/sonic/sonic.gd
@@ -1,25 +1,37 @@
 extends CharacterBody2D
 class_name Sonic
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -250.0
-
+@export var TOP_SPEED := 200.0
+@export var ACCEL := 800.0
+@export var INITIAL_JUMP_VELOCITY := -225.0
+@export var HELD_JUMP_VELOCITY := -200.0
+@export var FRICTION := 1000
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if Input.is_action_pressed("jump"):
+			velocity.y += HELD_JUMP_VELOCITY * delta
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = INITIAL_JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction != 0:
+		velocity.x = move_toward(
+			velocity.x,
+			direction * TOP_SPEED,
+			ACCEL * delta
+		)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(
+			velocity.x,
+			0,
+			FRICTION * delta
+		)
 
 	move_and_slide()
