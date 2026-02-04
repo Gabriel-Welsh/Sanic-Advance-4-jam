e065c2781553c5116abd2ba8d08dd1d24c1445a7
Gabe-Welsh <gjwelsh42@tntech.edu>
2026-02-04 13:18:26 -0600

    sonic can move left and right better now

--- entities/sonic/sonic.gd
+++ entities/sonic/sonic.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 class_name Sonic
 
 @export var TOP_SPEED := 200.0
-@export var ACCEL := 800.0
+@export var ACCEL := 200.0
 @export var INITIAL_JUMP_VELOCITY := -225.0
 @export var HELD_JUMP_VELOCITY := -200.0
 @export var FRICTION := 1000
@@ -18,20 +18,26 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = INITIAL_JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction != 0:
-		velocity.x = move_toward(
-			velocity.x,
-			direction * TOP_SPEED,
-			ACCEL * delta
-		)
+		if direction != velocity.normalized().x && velocity.x != 0:
+			velocity.x = move_toward(
+				velocity.x,
+				direction * TOP_SPEED,
+				ACCEL * delta + FRICTION*delta
+			)
+		else:
+			velocity.x = move_toward(
+				velocity.x,
+				direction * TOP_SPEED,
+				ACCEL * delta
+			)
 	else:
 		velocity.x = move_toward(
 			velocity.x,
 			0,
 			FRICTION * delta
 		)
+	print(velocity)
 
 	move_and_slide()
